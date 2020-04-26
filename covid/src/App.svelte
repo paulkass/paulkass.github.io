7692c6d68b74a5d5d2169bbed05d514d9f7eff4a
Paul Kassianik <pkassianik@gmail.com>
2020-04-26 15:59:58 -0700

    Added svelte site

---
+++ covid/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Chart from 'svelte-frappe-charts';
+
+    let data = {
+        labels: ["Shop", "School", "Work"],
+
+        datasets: [
+            { name: "Some Data", values: [25, 50, 25] }
+        ]
+    };
+
+</script>
+
+<h1>Will I get infected?</h1>
+
+<Chart data={data} type="percentage" />
